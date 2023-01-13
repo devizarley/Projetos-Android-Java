3af7ded6eb1a4100180b6d77a49946ac81a11d9c
Izarley Rodrigues <izarley.vac@hotmail.com>
2023-01-13 16:56:42 -0300

    att

---
+++ Organizze/app/src/main/java/izarleydev/com/organizze/activitys/Cadastro_Activity.java
@@ -0,0 +1,16 @@
+package izarleydev.com.organizze.activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import izarleydev.com.organizze.R;
+
+public class Cadastro_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
--- Organizze/app/src/main/java/izarleydev/com/organizze/activitys/MainActivity.java
+++ Organizze/app/src/main/java/izarleydev/com/organizze/activitys/MainActivity.java
@@ -1,12 +1,19 @@
-package izarleydev.com.organizze;
+package izarleydev.com.organizze.activitys;
+
+import static izarleydev.com.organizze.R.layout.activity_cadastro;
+import static izarleydev.com.organizze.R.layout.activity_login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.transition.Slide;
+import android.view.View;
 
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+
+import izarleydev.com.organizze.R;
 
 public class MainActivity extends IntroActivity {
 
@@ -36,5 +43,20 @@ public class MainActivity extends IntroActivity {
                 .background(R.color.white)
                 .fragment(R.layout.intro_4)
                 .build());
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.white)
+                .fragment(R.layout.intro_cadastro)
+                .build());
+    }
+
+    public void btEntrar(View view) {
+
+        startActivity(new Intent(this, Login_Activity.class ));
+
+    }
+    public void btCadastrar(View view) {
+
+        startActivity(new Intent(this, Cadastro_Activity.class));
+
     }
 }
