ccca2bd60ff73fdaa4af9fc32d9b771a8e50cb0a
Izarley Rodrigues <izarley.vac@hotmail.com>
2023-01-13 15:41:49 -0300

    att

--- Organizze/app/src/main/java/izarleydev/com/organizze/MainActivity.java
+++ Organizze/app/src/main/java/izarleydev/com/organizze/MainActivity.java
@@ -3,14 +3,38 @@ package izarleydev.com.organizze;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.transition.Slide;
 
-public class MainActivity extends AppCompatActivity {
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+
+public class MainActivity extends IntroActivity {
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.white)
+                .fragment(R.layout.intro_1)
+                .build());
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.white)
+                .fragment(R.layout.intro_2)
+                .build());
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.white)
+                .fragment(R.layout.intro_3)
+                .build());
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.white)
+                .fragment(R.layout.intro_4)
+                .build());
     }
 }
