277ff504ce0c5237693c123f403837199febbdff
Izarley Rodrigues <izarley.vac@hotmail.com>
2023-01-17 17:50:29 -0300

    at

--- WhatsApp/app/src/main/java/izarleydev/com/whatsapp/Activitys/Cadastro.java
+++ WhatsApp/app/src/main/java/izarleydev/com/whatsapp/Activitys/Cadastro.java
@@ -1,16 +1,29 @@
 package izarleydev.com.whatsapp.Activitys;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.core.view.View;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import izarleydev.com.whatsapp.R;
 
 public class Cadastro extends AppCompatActivity {
 
     /*Toda a configuração de cadastro de usuarios*/
+    TextInputEditText inputEmail;
+    TextInputEditText inputSenha;
+    TextInputEditText inputSenhaC;
+    private StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+    private FirebaseAuth usuario = FirebaseAuth.getInstance();
 
 
 
@@ -18,8 +31,22 @@ public class Cadastro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        inputEmail = findViewById(R.id.inputEmail);
+        inputSenha = findViewById(R.id.inputSenha);
+        inputSenhaC = findViewById(R.id.inputSenhaC);
+
+
     }
     public void buttonCadastro (View view) {
+
+        usuario.createUserWithEmailAndPassword(inputEmail.toString(), inputSenha.toString())
+                        .addOnCompleteListener(Cadastro.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            }
+                        });
 
     }
 }
--- WhatsApp/app/src/main/java/izarleydev/com/whatsapp/Activitys/MainActivity.java
+++ WhatsApp/app/src/main/java/izarleydev/com/whatsapp/Activitys/MainActivity.java
@@ -4,19 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import com.google.firebase.database.core.view.View;
+import com.google.android.material.textfield.TextInputEditText;
 
 import izarleydev.com.whatsapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
     /*Toda configuração geral*/
+    Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.teste);
+
+
+    }
+
+    public void btnavegar (View view) {
+        startActivity(new Intent(MainActivity.this, Cadastro.class));
     }
 
 }
