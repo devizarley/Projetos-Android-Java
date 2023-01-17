eb7743c7dc595e5a6646c958d97b6600c0a35a9e
Izarley Rodrigues <izarley.vac@hotmail.com>
2023-01-17 15:43:18 -0300

    att

--- WhatsApp/app/src/main/java/izarleydev/com/whatsapp/Activitys/Cadastro.java
+++ WhatsApp/app/src/main/java/izarleydev/com/whatsapp/Activitys/Cadastro.java
@@ -4,15 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.core.view.View;
+
 import izarleydev.com.whatsapp.R;
 
 public class Cadastro extends AppCompatActivity {
 
     /*Toda a configuração de cadastro de usuarios*/
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+    }
+    public void buttonCadastro (View view) {
+
     }
 }
--- WhatsApp/app/src/main/java/izarleydev/com/whatsapp/Activitys/MainActivity.java
+++ WhatsApp/app/src/main/java/izarleydev/com/whatsapp/Activitys/MainActivity.java
@@ -18,4 +18,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
