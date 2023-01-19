8abc2f52546330654f2bd13242473c976973038f
Izarley Rodrigues <izarley.vac@hotmail.com>
2023-01-19 15:01:49 -0300

    att

--- WhatsappClone/app/src/main/java/com/example/whatsappclone/MainActivity.java
+++ WhatsappClone/app/src/main/java/com/example/whatsappclone/MainActivity.java
@@ -1,17 +1,14 @@
-package izarleydev.com.organizze.activitys;
+package com.example.whatsappclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import izarleydev.com.organizze.R;
-
-
-public class Cadastro_Activity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cadastro);
+        setContentView(R.layout.activity_main);
     }
 }
