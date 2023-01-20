f97ad7463ca5904366174b19680afc77f5ea0806
Izarley Rodrigues <izarley.vac@hotmail.com>
2023-01-20 18:35:46 -0300

    att

--- Organizze/app/src/main/java/izarleydev/com/organizze/activitys/activity/FirstFragment.java
+++ Organizze/app/src/main/java/izarleydev/com/organizze/activitys/activity/FirstFragment.java
@@ -9,7 +9,8 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-import izarleydev.com.organizze.activitys.activity.databinding.FragmentFirstBinding;
+import izarleydev.com.organizze.R;
+import izarleydev.com.organizze.databinding.FragmentFirstBinding;
 
 public class FirstFragment extends Fragment {
 
