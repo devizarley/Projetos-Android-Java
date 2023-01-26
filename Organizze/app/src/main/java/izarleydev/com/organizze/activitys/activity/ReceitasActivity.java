8d281b2eb37ed14324614e67cd9e46f067c3ac9c
Izarley Rodrigues <izarley.vac@hotmail.com>
2023-01-26 16:22:26 -0300

    att

--- Organizze/app/src/main/java/izarleydev/com/organizze/activitys/activity/ReceitasActivity.java
+++ Organizze/app/src/main/java/izarleydev/com/organizze/activitys/activity/ReceitasActivity.java
@@ -3,14 +3,28 @@ package izarleydev.com.organizze.activitys.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 import izarleydev.com.organizze.R;
+import izarleydev.com.organizze.activitys.Helper.DateCustom;
 
 public class ReceitasActivity extends AppCompatActivity {
+
+    private TextInputEditText campoData, campoCategoria, campoDescricao;
+    private EditText campoValor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receitas);
+
+        campoData = findViewById(R.id.editData);
+        campoCategoria = findViewById(R.id.editCategoria);
+        campoDescricao = findViewById(R.id.editDescricao);
+        campoValor = findViewById(R.id.editValor);
+
+        campoData.setText(DateCustom.dataAtual());
     }
 }
