119311cbd32f12c61e9bd774274ae211ac4483f6
Izarley Rodrigues <izarley.vac@hotmail.com>
2023-01-26 16:04:53 -0300

    Create DateCustom.java

---
+++ Organizze/app/src/main/java/izarleydev/com/organizze/activitys/Helper/DateCustom.java
@@ -0,0 +1,13 @@
+package izarleydev.com.organizze.activitys.Helper;
+
+import java.text.SimpleDateFormat;
+
+public class DateCustom {
+
+    public static String dataAtual(){
+        long date = System.currentTimeMillis();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        String dataString = simpleDateFormat.format(date);
+        return dataString;
+    }
+}
