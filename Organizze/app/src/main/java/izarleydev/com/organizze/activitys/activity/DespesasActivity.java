052044303bd786693b28cdbf1e41ab3f88459541
Izarley Rodrigues <izarley.vac@hotmail.com>
2023-01-29 15:48:18 -0300

    Update DespesasActivity.java

--- Organizze/app/src/main/java/izarleydev/com/organizze/activitys/activity/DespesasActivity.java
+++ Organizze/app/src/main/java/izarleydev/com/organizze/activitys/activity/DespesasActivity.java
@@ -14,6 +14,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import izarleydev.com.organizze.R;
