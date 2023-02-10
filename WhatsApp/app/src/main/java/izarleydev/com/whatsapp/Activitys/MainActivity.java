dc84946ab69ae047bed9a31b0ce47f3bd731f82f
Izarley Rodrigues <izarley.vac@hotmail.com>
2023-02-10 17:18:48 -0300

    att

--- WhatsApp/app/src/main/java/izarleydev/com/whatsapp/Activitys/MainActivity.java
+++ WhatsApp/app/src/main/java/izarleydev/com/whatsapp/Activitys/MainActivity.java
@@ -21,10 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        button = findViewById(R.id.teste);
-
-
+        
     }
 
     public void btnavegar (View view) {
