3dcb362a831e4fab3b500259be1589f9ba9758fa
Izarley Rodrigues <izarley.vac@hotmail.com>
2023-02-11 09:36:55 -0300

    att

--- WhatsApp/app/src/main/java/izarleydev/com/whatsapp/Activitys/activitys/Login.java
+++ WhatsApp/app/src/main/java/izarleydev/com/whatsapp/Activitys/activitys/Login.java
@@ -1,5 +1,6 @@
 package izarleydev.com.whatsapp.Activitys.activitys;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,14 +8,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
+import izarleydev.com.whatsapp.Activitys.config.ConfigFirebase;
 import izarleydev.com.whatsapp.Activitys.model.Usuario;
 import izarleydev.com.whatsapp.R;
 
 public class Login extends AppCompatActivity {
 
     private TextInputEditText fieldEmail, fieldSenha;
+    private FirebaseAuth auth = ConfigFirebase.getAuth();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +32,32 @@ public class Login extends AppCompatActivity {
         fieldSenha = findViewById(R.id.inputLoginSenha);
     }
 
-    public void loginUser (View view){
+    public void loginUser (Usuario usuario){
+        auth.signInWithEmailAndPassword(usuario.getEmail(), usuario.getSenha()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+
+                    startActivity(new Intent(Login.this, MainActivity.class));
+
+                }else {
+                    Toast.makeText(Login.this, "Erro ao autenticar usuario!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void validateFildUser (View view){
         String valueEmail = fieldEmail.getText().toString();
         String valueSenha = fieldSenha.getText().toString();
 
         if (!valueEmail.isEmpty()){
             if (!valueSenha.isEmpty()){
+                Usuario usuario = new Usuario();
+                usuario.setEmail(valueEmail);
+                usuario.setSenha(valueSenha);
 
-
+                loginUser(usuario);
 
             }else {
                 Toast.makeText(this, "Preencha a senha!", Toast.LENGTH_SHORT).show();
