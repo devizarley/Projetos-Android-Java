c16805d632e460cfa1b31be2c7a35fcf4af00534
Izarley Rodrigues <izarley.vac@hotmail.com>
2023-02-11 07:48:19 -0300

    atty

--- WhatsApp/app/src/main/java/izarleydev/com/whatsapp/Activitys/activitys/Cadastro.java
+++ WhatsApp/app/src/main/java/izarleydev/com/whatsapp/Activitys/activitys/Cadastro.java
@@ -3,7 +3,7 @@ package izarleydev.com.whatsapp.Activitys.activitys;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Bitmap;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -12,7 +12,6 @@ import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.core.view.View;
 
 import izarleydev.com.whatsapp.Activitys.config.ConfigFirebase;
@@ -20,25 +19,20 @@ import izarleydev.com.whatsapp.R;
 
 public class Cadastro extends AppCompatActivity {
 
-    /*Toda a configuração de cadastro de usuarios*/
-    TextInputEditText inputEmail;
-    TextInputEditText inputSenha;
-    TextInputEditText inputSenhaC;
+    TextInputEditText inputEmail, inputName, inputSenha;
     private DatabaseReference ref = ConfigFirebase.getFirebaseDatabase();
     private FirebaseAuth auth = ConfigFirebase.getAuth();
 
-
-
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
 
+        inputName = findViewById(R.id.inputName);
         inputEmail = findViewById(R.id.inputEmail);
         inputSenha = findViewById(R.id.inputSenha);
-        inputSenhaC = findViewById(R.id.inputSenhaC);
-
-
+        
     }
     public void buttonCadastro (View view) {
 
@@ -49,7 +43,7 @@ public class Cadastro extends AppCompatActivity {
         if (valueInputEmail.isEmpty()){
 
         }else {
-            
+
         }
 
 
