9198f2fc9da4cc0e5d3f942879f317aae72f75c9
Izarley Rodrigues <izarley.vac@hotmail.com>
2023-02-12 11:37:11 -0300

    att colors

--- WhatsApp/app/src/main/java/izarleydev/com/whatsapp/Activitys/activitys/MainActivity.java
+++ WhatsApp/app/src/main/java/izarleydev/com/whatsapp/Activitys/activitys/MainActivity.java
@@ -1,13 +1,22 @@
 package izarleydev.com.whatsapp.Activitys.activitys;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toolbar;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import izarleydev.com.whatsapp.Activitys.config.ConfigFirebase;
 import izarleydev.com.whatsapp.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth auth = ConfigFirebase.getAuth();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +26,30 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setTitle("WhatsApp");
         setSupportActionBar(toolbar);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menuSair:
+                singOut();
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    public void singOut (){
+        try {
+            auth.signOut();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
