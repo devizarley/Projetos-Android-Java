a01b499fb2269e6cab4ea59c34e8b2582cfc620a
Izarley Rodrigues <izarley.vac@hotmail.com>
2023-02-16 10:37:04 -0300

    att permissons

--- WhatsApp/app/src/main/java/izarleydev/com/whatsapp/Activitys/activitys/Configuracoes.java
+++ WhatsApp/app/src/main/java/izarleydev/com/whatsapp/Activitys/activitys/Configuracoes.java
@@ -2,11 +2,17 @@ package izarleydev.com.whatsapp.Activitys.activitys;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.os.Bundle;
 
 import izarleydev.com.whatsapp.R;
 
 public class Configuracoes extends AppCompatActivity {
+    
+    private String[] permissions = new String[]{
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
