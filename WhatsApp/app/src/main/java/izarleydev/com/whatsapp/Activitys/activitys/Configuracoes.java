27ff617f047b2b57aa3ca5889c60d3780b88bac7
Izarley Rodrigues <izarley.vac@hotmail.com>
2023-02-16 11:07:39 -0300

    validacoes

--- WhatsApp/app/src/main/java/izarleydev/com/whatsapp/Activitys/activitys/Configuracoes.java
+++ WhatsApp/app/src/main/java/izarleydev/com/whatsapp/Activitys/activitys/Configuracoes.java
@@ -1,8 +1,12 @@
 package izarleydev.com.whatsapp.Activitys.activitys;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import izarleydev.com.whatsapp.Activitys.helper.Permissao;
@@ -28,5 +32,34 @@ public class Configuracoes extends AppCompatActivity {
 
         //Validar permissões
         Permissao.validarPermissoes(permissions, this, 1);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        for (int permissaoResultado : grantResults){
+            if (permissaoResultado == PackageManager.PERMISSION_DENIED){
+                alertaValidacaoPermissao();
+            };
+        }
+
+    }
+    private void alertaValidacaoPermissao(){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissões Negadas");
+        builder.setMessage("Para utilizar o app é necessário aceitas as permissões");
+        
+        builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
     }
 }
