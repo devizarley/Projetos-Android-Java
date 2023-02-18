c4e5d4b53f013968ff4cff6cef0a2f189df40fb8
Izarley Rodrigues <izarley.vac@hotmail.com>
2023-02-18 13:48:11 -0300

    att

--- WhatsApp/app/src/main/java/izarleydev/com/whatsapp/Activitys/activitys/Configuracoes.java
+++ WhatsApp/app/src/main/java/izarleydev/com/whatsapp/Activitys/activitys/Configuracoes.java
@@ -5,9 +5,14 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageButton;
 
 import izarleydev.com.whatsapp.Activitys.helper.Permissao;
 import izarleydev.com.whatsapp.R;
@@ -18,17 +23,35 @@ public class Configuracoes extends AppCompatActivity {
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.CAMERA
     };
+    private ImageButton imageButtonCamera, imageButtonGaleria;
+    private static final int SELECAO_CAMERA = 100;
+    private static final int SELECAO_GALERIA = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracoes);
 
+        imageButtonCamera = findViewById(R.id.imageButtonCamera);
+        imageButtonGaleria = findViewById(R.id.imageButtonGallery);
+
         androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Configurações");
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        imageButtonCamera.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("QueryPermissionsNeeded")
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(intent, SELECAO_CAMERA);
+                }
+            }
+        });
 
         //Validar permissões
         Permissao.validarPermissoes(permissions, this, 1);
