e2fd2818048f449475ca989f0c0986313222296d
Izarley Rodrigues <izarley.vac@hotmail.com>
2023-02-21 21:52:37 -0300

    Update ConfigFirebase.java

--- WhatsApp/app/src/main/java/izarleydev/com/whatsapp/Activitys/config/ConfigFirebase.java
+++ WhatsApp/app/src/main/java/izarleydev/com/whatsapp/Activitys/config/ConfigFirebase.java
@@ -3,10 +3,13 @@ package izarleydev.com.whatsapp.Activitys.config;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class ConfigFirebase {
     private static DatabaseReference database;
     private static FirebaseAuth auth;
+    private static StorageReference fbStorage;
 
     //retorna a instancia do FirebaseDatabase
     public static DatabaseReference getFirebaseDatabase(){
@@ -22,5 +25,13 @@ public class ConfigFirebase {
             auth = FirebaseAuth.getInstance();
         }
         return auth;
+    }
+
+    //retorna a instancia do FirebaseStorage
+    public static StorageReference getFirebaseStorage(){
+        if (fbStorage == null) {
+            fbStorage = FirebaseStorage.getInstance().getReference();
+        }
+        return fbStorage;
     }
 }
