a5a581842ac9fd04301371cbb4fe60cb3f1db0e0
Izarley Rodrigues <izarley.vac@hotmail.com>
2023-04-03 17:04:21 -0700

    att Organizze

--- Organizze/app/src/main/java/izarleydev/com/organizze/activitys/activity/MainActivity.java
+++ Organizze/app/src/main/java/izarleydev/com/organizze/activitys/activity/MainActivity.java
@@ -47,6 +47,7 @@ public class MainActivity extends IntroActivity {
         addSlide(new FragmentSlide.Builder()
                 .background(R.color.white)
                 .fragment(R.layout.intro_cadastro)
+                .canGoForward(false)
                 .build());
     }
 
