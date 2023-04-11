e50b24924193a724bcab2e81f96d1873f955e55b
Izarley Rodrigues <izarley.vac@hotmail.com>
2023-04-10 18:39:06 -0700

    att

--- Instagram/app/src/main/java/izarleydev/com/instagram/activity/MainActivity.java
+++ Instagram/app/src/main/java/izarleydev/com/instagram/activity/MainActivity.java
@@ -4,14 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 import izarleydev.com.instagram.R;
+import izarleydev.com.instagram.helper.ConfigFirebase;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseAuth auth ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = ConfigFirebase.getAuth();
+        auth.getCurrentUser();
+        auth.signOut();
     }
 
 }
