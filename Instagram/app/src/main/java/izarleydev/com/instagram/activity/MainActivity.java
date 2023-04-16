c79af1ec140be8edccb91d11e23f59f49e91fd2e
Izarley Rodrigues <izarley.vac@hotmail.com>
2023-04-15 21:12:15 -0300

    BottomNavigation create Instagram

--- Instagram/app/src/main/java/izarleydev/com/instagram/activity/MainActivity.java
+++ Instagram/app/src/main/java/izarleydev/com/instagram/activity/MainActivity.java
@@ -2,6 +2,9 @@ package izarleydev.com.instagram.activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
@@ -11,54 +14,54 @@ import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-import java.util.zip.Inflater;
-
 import izarleydev.com.instagram.R;
+import izarleydev.com.instagram.databinding.ActivityMainBinding;
+import izarleydev.com.instagram.fragment.AccountFragment;
+import izarleydev.com.instagram.fragment.AddFragment;
+import izarleydev.com.instagram.fragment.HomeFragment;
+import izarleydev.com.instagram.fragment.SearchFragment;
 import izarleydev.com.instagram.helper.ConfigFirebase;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth auth ;
-    androidx.appcompat.widget.Toolbar toolbar;
+    ActivityMainBinding binding;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        toolbar = findViewById(R.id.toolbar);
-        toolbar.setTitle("");
-        setSupportActionBar(toolbar);
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigationView.setBackground(null);
 
+        binding.bottomNavigationView.setOnItemSelectedListener( item -> {
+
+            switch (item.getItemId()) {
+                case R.id.ic_home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.ic_pesquisa:
+                    replaceFragment(new SearchFragment());
+                    break;
+                case R.id.ic_account:
+                    replaceFragment(new AccountFragment());
+                    break;
+            }
+
+            return true;
+
+        });
         auth = ConfigFirebase.getAuth();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
 
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
-
-        return super.onCreateOptionsMenu(menu);
+    private void replaceFragment (Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
-        switch (item.getItemId()){
-            case R.id.sair:
-                signOut();
-                finish();
-                break;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void signOut (){
-        try {
-            auth.signOut();
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
 }
---
+++ Instagram/app/src/main/java/izarleydev/com/instagram/fragment/HomeFragment.java
@@ -0,0 +1,26 @@
+package izarleydev.com.instagram.fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import izarleydev.com.instagram.R;
+
+public class HomeFragment extends Fragment {
+    androidx.appcompat.widget.Toolbar toolbar;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
