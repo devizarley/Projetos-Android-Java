3554e78f177dcc186a6f6c7aad971fd6caad9b76
Izarley Rodrigues <izarley.vac@hotmail.com>
2023-05-15 15:22:31 -0300

    create new dialogDown Instagram

--- Instagram/app/src/main/java/izarleydev/com/instagram/activity/MainActivity.java
+++ Instagram/app/src/main/java/izarleydev/com/instagram/activity/MainActivity.java
@@ -11,21 +11,27 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
@@ -40,7 +46,6 @@ import izarleydev.com.instagram.fragment.HomeFragment;
 import izarleydev.com.instagram.fragment.SearchFragment;
 import izarleydev.com.instagram.helper.ConfigFirebase;
 import izarleydev.com.instagram.helper.Permissao;
-import izarleydev.com.instagram.helper.UsuarioFirebase;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth auth ;
@@ -48,9 +53,11 @@ public class MainActivity extends AppCompatActivity {
     private FloatingActionButton ic_add;
     private Toolbar toolbar2;
     private String[] permissoesNecessarias = new String[]{
+            Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.CAMERA
     };
     private static final int SELECAO_CAMERA = 100;
+    private static final int SELECAO_GALERIA = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,11 +70,12 @@ public class MainActivity extends AppCompatActivity {
         toolbar2 = findViewById(R.id.toolbar2);
         toolbar2.setTitle("");
         setSupportActionBar(toolbar2);
+
         Permissao.validarPermissoes(permissoesNecessarias, this, 1);
 
-        FloatingActionButton fab = findViewById(R.id.ic_add);
+        /*FloatingActionButton fab = findViewById(R.id.ic_add);
         fab.setImageResource(R.drawable.ic_add_white);
-        fab.setColorFilter(ContextCompat.getColor(this, R.color.fabStyle), PorterDuff.Mode.SRC_IN);
+        fab.setColorFilter(ContextCompat.getColor(this, R.color.fabStyle), PorterDuff.Mode.SRC_IN);*/
 
         replaceFragment(new HomeFragment());
         binding.bottomNavigationView.setBackground(null);
@@ -76,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
         ic_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                replaceFragment(new AddFragment());
+                showBottomDialog();
             }
         });
 
@@ -93,7 +101,10 @@ public class MainActivity extends AppCompatActivity {
                     replaceFragment(new AccountFragment());
                     break;
                 case R.id.ic_cam:
-                        replaceFragment(new AddFragment());
+                    Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    if (i.resolveActivity(getApplicationContext().getPackageManager()) != null){
+                        startActivityForResult(i,SELECAO_CAMERA);
+                    }
                     break;
             }
 
@@ -131,6 +142,7 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Instagram");
         builder.setMessage("Deseja realmente deslogar?");
+
         builder.setPositiveButton("Confirmar",
                 new DialogInterface.OnClickListener() {
                     @Override
@@ -140,6 +152,7 @@ public class MainActivity extends AppCompatActivity {
                         finish();
                     }
                 });
+
         builder.setNegativeButton("Cancelar",
                 new DialogInterface.OnClickListener() {
                     @Override
@@ -159,4 +172,102 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.commit();
     }
 
+    private void showBottomDialog() {
+
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.bottomsheetlayout);
+
+        LinearLayout storiesLayout = dialog.findViewById(R.id.layoutStories);
+        LinearLayout feedLayout = dialog.findViewById(R.id.layoutFeed);
+        LinearLayout liveLayout = dialog.findViewById(R.id.layoutLive);
+        ImageView cancelButton = dialog.findViewById(R.id.cancelButton);
+
+        storiesLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dialog.dismiss();
+                Toast.makeText(MainActivity.this,"Esta função está em construção!",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        feedLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                if (i.resolveActivity(getApplicationContext().getPackageManager()) != null){
+                    startActivityForResult(i, SELECAO_GALERIA);
+                }
+
+                dialog.dismiss();
+
+            }
+        });
+
+        liveLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dialog.dismiss();
+                Toast.makeText(MainActivity.this,"Esta função está em construção!",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
+
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if ( resultCode == this.RESULT_OK ){
+
+            Bitmap imagem = null;
+
+            try {
+                //validar tipo de selcao
+                switch (requestCode){
+                    case SELECAO_CAMERA:
+                        imagem = (Bitmap) data.getExtras().get("data");
+                        break;
+                    case SELECAO_GALERIA:
+                        Uri localImagem = data.getData();
+                        imagem = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), localImagem);
+                        break;
+                }
+
+                //validar imagem selecionada
+
+                if (imagem != null){
+                    //converte imagem em byte array
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    imagem.compress(Bitmap.CompressFormat.JPEG, 70, baos);
+                    byte[] dadosImagem = baos.toByteArray();
+
+                    //envia imagem escolhida para aplicação de filtro
+                    Intent i = new Intent(getApplicationContext(), FiltroActivity.class);
+                    i.putExtra("fotoEscolhida", dadosImagem);
+                    startActivity(i);
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+    }
 }
